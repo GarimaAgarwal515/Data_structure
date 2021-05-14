ef5f7c3e3417291532a248cf881d55bd52d331f2
GarimaAgarwal515 <69511820+GarimaAgarwal515@users.noreply.github.com>
2021-05-14 11:52:47 +0530

    Add files via upload

---
+++ StackArrayRepresentationMain.java
@@ -0,0 +1,19 @@
+package ArrayRepresentation;
+
+public class StackArrayRepresentationMain {
+    public static void main(String[] args) {
+
+
+        StackArrayRepresentation obj = new StackArrayRepresentation(5);
+        obj.push(10);
+        obj.push(20);
+        obj.push(30);
+        obj.push(40);
+        obj.push(50);
+        obj.pop();
+        obj.Display();
+
+        
+    }
+
+}
